500df95cf06e394311b178c8cf66b965b0ffb09f
harrier999 <harrier999@naver.com>
2022-12-27 17:44:15 +0900

    [TINY] Change example address and delete prints

--- api_demo/front.py
+++ api_demo/front.py
@@ -8,7 +8,7 @@ def main():
 	
 	
 	
-	input_address = st.text_input("Your address here", "0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb")
+	input_address = st.text_input("Your address here", "0xa06235f755cb24eb896eb617d3fa7c8f372f04cc674d39c18da545b380b94277")
 	# good examples
 	# 0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb
 	# 0xa06235f755cb24eb896eb617d3fa7c8f372f04cc674d39c18da545b380b94277
@@ -22,7 +22,6 @@ def main():
 		json_data = response.json()
 		for token in json_data:
 			num +=1
-			print(token)
 			token_id_list.append(token['token_data_id_hash'])
 			if num > 50:
 				break
@@ -34,14 +33,12 @@ def main():
    
 			response = requests.get(get_token_metadata_URL)
 			json_data = response.json()
-			print(json_data)
 
 			st.text(json_data[0]['collection_name'])
 			st.text(json_data[0]['name'])
 			
 			st.image(json_data[0]['metadata_uri'])
 			# st.image('https://cloudflare-ipfs.com/ipfs/QmQ7m9tAFapj13h55u9iRQ4FtaiTAKtSFSgY3As47DMvfu\n')
-			print(json_data[0]['metadata_uri'])
 
 
 
