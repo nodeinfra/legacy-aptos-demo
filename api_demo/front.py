f865f830dfdcd47500e355d31bc12b4a07022d38
harrier999 <harrier999@naver.com>
2022-12-28 14:48:23 +0900

    [FIX] add metadata uri conditions

--- api_demo/front.py
+++ api_demo/front.py
@@ -13,15 +13,13 @@ COLUMN_SIZE = 3
 
 def main():
 	print('------------new page-------------')
-
 	token_id_list = list()
-	metadata_list = list()
-
 	st.set_page_config(
 		page_title="NodeInfra",
 		page_icon="./imgs/dd.png"
 	)
 	st.title("Aptos Market")
+
 	input_address = st.text_input("Your address here", example_address)
 	left, right = st.columns(2)
 	button = left.button('Browse it!')
@@ -39,27 +37,20 @@ def main():
 		for i, token_id in enumerate(token_id_list):
 			response = requests.get(api_address+get_nft_metadata, params={'nft_hash':token_id})
 			json_data = response.json()[0]
+			print(json_data)
 			if slow_images:
-				if json_data['metadata_uri'].startswith('https://nftstorage') and not json_data['metadata_uri'].endswith('.jpeg'):
-					
-					# json_data['metadata_uri']='./imgs/aptos.png'
+				metadata_uri = json_data['metadata_uri']
+				if (metadata_uri.startswith('https://nftstorage') and not (metadata_uri.endswith('.jpeg') or metadata_uri.endswith('gif') or metadata_uri.endswith('jpg')))or (not metadata_uri.startswith('ipfs') and metadata_uri.endswith('json')):
 					response = requests.get(json_data['metadata_uri']).json()
 					print(response)
 					json_data['metadata_uri'] = response['image']
-
-			
-
-
-			# st.text(json_data['collection_name'])
-			# st.text(json_data['name'])
-			# st.image(json_data['metadata_uri'])
-			# st.image('https://cloudflare-ipfs.com/ipfs/QmQ7m9tAFapj13h55u9iRQ4FtaiTAKtSFSgY3As47DMvfu\n')
-
 			if i % COLUMN_SIZE == 0:
 				col_list = st.columns(COLUMN_SIZE)
-			#col_list[i%COLUMN_SIZE].text(metadata['collection_name'])
 			col_list[i%COLUMN_SIZE].text(json_data['name'])
 			col_list[i%COLUMN_SIZE].image(json_data['metadata_uri'])
-			# print(metadata)
+
+	st.subheader('sample addresses')
+	st.code('0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb')
+	st.code('0x87c5241e5d6eb8ba7cd45684e978c8b61d78041662d1c209491104593a60455e')
 if __name__ == "__main__":
 	main()  
