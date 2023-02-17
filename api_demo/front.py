baa451638ebc2121c159d034026beb66a68088e9
harrier999 <harrier999@naver.com>
2023-02-17 16:12:20 +0900

    [UPDATE] Update to adapt to the new version of the API.

--- api_demo/front.py
+++ api_demo/front.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import requests
 
-api_address = "http://223.130.135.191:3000/rpc/"
-get_nft_balances = 'get_nft_balances'
-get_nft_metadata = 'get_nft_metadata'
-example_address = "0xa06235f755cb24eb896eb617d3fa7c8f372f04cc674d39c18da545b380b94277"
+api_address = "http://aptos-mainnet.nodeinfra.com:3123/"
+nfts_endpoint = 'nfts'
+example_address = "0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb"
 	# good examples
 	# 0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb
 	# 0xa06235f755cb24eb896eb617d3fa7c8f372f04cc674d39c18da545b380b94277
 	# 0x87c5241e5d6eb8ba7cd45684e978c8b61d78041662d1c209491104593a60455e
 COLUMN_SIZE = 3
+MAX_NFTS = 30
 
 def main():
 	print('------------new page-------------')
@@ -27,30 +27,26 @@ def main():
 
 	num = 0
 	if button:
-		response = requests.get(api_address+get_nft_balances, params={'owner_hash':input_address})
+		response = requests.get(api_address+nfts_endpoint, params={'owner_address':input_address})
 		json_data = response.json()
-		for token in json_data:
-			num +=1
-			token_id_list.append(token['token_data_id_hash'])
-			if num > 20:
-				break
-		for i, token_id in enumerate(token_id_list):
-			response = requests.get(api_address+get_nft_metadata, params={'nft_hash':token_id})
-			json_data = response.json()[0]
-			print(json_data)
-			if slow_images:
-				metadata_uri = json_data['metadata_uri']
-				if (metadata_uri.startswith('https://nftstorage') and not (metadata_uri.endswith('.jpeg') or metadata_uri.endswith('gif') or metadata_uri.endswith('jpg')))or (not metadata_uri.startswith('ipfs') and metadata_uri.endswith('json')):
-					response = requests.get(json_data['metadata_uri']).json()
-					print(response)
-					json_data['metadata_uri'] = response['image']
-			if i % COLUMN_SIZE == 0:
-				col_list = st.columns(COLUMN_SIZE)
-			col_list[i%COLUMN_SIZE].text(json_data['name'])
-			col_list[i%COLUMN_SIZE].image(json_data['metadata_uri'])
+		nfts = json_data["nfts"]
 
+		
+		for index, nft in enumerate(nfts):
+			if index % COLUMN_SIZE == 0:
+				col_list = st.columns(COLUMN_SIZE)
+			metadata_uri = nft['metadata_uri']
+			if slow_images and (metadata_uri.startswith('https://nftstorage') and not (metadata_uri.endswith('.jpeg') or metadata_uri.endswith('gif') or metadata_uri.endswith('jpg')))or (not metadata_uri.startswith('ipfs') and metadata_uri.endswith('json')):
+				response = requests.get(nft['metadata_uri']).json()
+				nft['metadata_uri'] = response['image']
+			col_list[index%COLUMN_SIZE].text(nft['name'])
+			col_list[index%COLUMN_SIZE].image(nft['metadata_uri'])
+   
+			if index == MAX_NFTS:
+				
+				break
 	st.subheader('sample addresses')
-	st.code('0x863e6d2f9aaa418b253d037a61027ecb9041f31d8d419f1819d83b26f43e16bb')
-	st.code('0x87c5241e5d6eb8ba7cd45684e978c8b61d78041662d1c209491104593a60455e')
+	st.code("0xa06235f755cb24eb896eb617d3fa7c8f372f04cc674d39c18da545b380b94277")
+	st.code("0x87c5241e5d6eb8ba7cd45684e978c8b61d78041662d1c209491104593a60455e")
 if __name__ == "__main__":
 	main()  
